b96c770773f5ad54988e2e93fdf325d1ca8ba9ce
Karan V <karan.e0119039@sret.edu.in>
2021-11-15 19:38:25 +0530

    t3

--- denochat-api/index.js
+++ denochat-api/index.js
@@ -1,8 +1,8 @@
-addEventListener("fetch", (e) => {
-	const res = new Response("Hello World Karan", {
-		headers: {
-			"content-type": "text/plain",
-		},
-	});
-	e.respondWith(res);
+import { Application } from "https://deno.land/x/oadk/mod.ts";
+
+const app = new Application();
+app.use((ctx) => {
+	ctx.response.body = "Hello Deno";
 });
+
+addEventListener("fetch", app.fetchEventHandler());
